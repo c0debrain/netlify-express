3c54c3def6d9a5938acb8c0ddcc4e843e166060d
shahin <codebrain@gmail.com>
2021-04-28 16:28:32 +1000

    server file

--- express/server.js
+++ express/server.js
@@ -11,6 +11,7 @@ router.get('/', (req, res) => {
   // res.writeHead(200, { 'Content-Type': 'text/html' });
   // res.write('<h1>Hello from Express.js!!</h1>');
   // res.end();
+  console.log("index")
   res.sendFile(path.join(__dirname + '/../app/index.html'));
 });
 
@@ -25,15 +26,10 @@ router.get('/test', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-router.get('/test', (req, res) => {
-  console.log("getting test")
-  //res.sendFile(path.join(__dirname + '../app/test.html'));
-});
-
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/test', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../app/index.html')));
+app.use('/test', (req, res) => res.sendFile(path.join(__dirname, '../app/test.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
