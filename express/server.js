e3bcfbc24aa07b8c21d8ce06ce69e0be870594e1
shahin <codebrain@gmail.com>
2021-04-28 17:00:54 +1000

    server file

--- express/server.js
+++ express/server.js
@@ -8,11 +8,11 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // res.writeHead(200, { 'Content-Type': 'text/html' });
-  // res.write('<h1>Hello from Express.js!!</h1>');
-  // res.end();
-  console.log("index")
-  res.sendFile(path.join(__dirname + '../app/index.html'));
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!!</h1>');
+  res.end();
+  // console.log("index")
+  // res.sendFile(path.join(__dirname + '../app/index.html'));
 });
 
 router.get('/test', (req, res) => {
